faf1693ce34ec28978e6536258ef4efdc75b2dc8
João Matias <jmatiasmn@gmail.com>
2023-07-09 11:22:12 -0300

    Plugin Basics 01

---
+++ cbb/cbb/Commands/TagWallLayersCommand.cs
@@ -0,0 +1,55 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cbb
+{
+    /// <summary>
+    /// Coomand code to be executed when button is clicked
+    /// </summary>
+
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class TagWallLayersCommand : IExternalCommand
+    {
+        /// <summary>
+        /// Tag wall layers by creating text note element on user specified point and populate it with information
+        /// </summary>
+        /// <param name="commandData"></param>
+        /// <param name="message"></param>
+        /// <param name="elements"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        /// 
+
+        #region Public properties
+        #endregion
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            string walls = "";
+
+            foreach (Element element in new FilteredElementCollector(commandData.Application.ActiveUIDocument.Document).OfCategory(BuiltInCategory.OST_Walls).WhereElementIsElementType())
+            {
+                walls += element.Name + "\n";
+            }
+            
+            TaskDialog diag = new TaskDialog("Test")
+            {
+                MainContent = walls,
+                MainInstruction = "aaa",
+                CommonButtons = TaskDialogCommonButtons.Yes,
+
+            };
+            diag.Show();
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ cbb/cbb/Main.cs
@@ -0,0 +1,63 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace cbb
+{
+    /// <summary>
+    /// Plugin's main entry point
+    /// </summary>
+    public class Main : IExternalApplication
+    {
+        #region external application public methods
+        /// <summary>
+        /// Called when Revit Shutdown
+        /// </summary>
+        /// <param name="application"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+        /// <summary>
+        /// Caled when Revit starts up.
+        /// </summary>
+        /// <param name="application"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Result OnStartup(UIControlledApplication application)
+        {
+            //Plugin's maing tab name;
+            string tabName = "Circle's Bim Blog";
+
+            //Panel name hosted on ribbon tab.
+            string pannelAnotationName = "Annotation Commnds";
+
+            //Create Tab on Revit UI.
+            application.CreateRibbonTab(tabName);
+
+            //Create first panel on Revit Ribbon tab;
+           var panelAnnotation = application.CreateRibbonPanel(tabName, pannelAnotationName);
+
+            //Create push button data and populate it with information.
+            var tagWallLayersBtnData = new PushButtonData("TagWallLayersBtnData", "Tag Wall \nLayers", Assembly.GetExecutingAssembly().Location, "cbb.TagWallLayersCommand")
+            {
+                ToolTipImage = new BitmapImage(new Uri(@"C:\Users\jmati\Desktop\Marko Koljancic\res\wall.png")),
+                ToolTip = "This is some sample tooltip text, replace it later"
+            };
+
+            var TagWallLayersBtn = panelAnnotation.AddItem(tagWallLayersBtnData) as PushButton;
+            TagWallLayersBtn.LargeImage = new BitmapImage(new Uri(@"C:\Users\jmati\Desktop\Marko Koljancic\res\32.jpg"));
+
+
+            return Result.Succeeded;
+        }
+        #endregion
+    }
+}
